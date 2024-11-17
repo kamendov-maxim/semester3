b830a06c6e7ce9d7e7e72807287264f7ce0b5842
kamendov-maxim <kamendov.maksim@icloud.com>
2024-11-17 13:54:35 +0300

    Tests

---
+++ MyNUnit/TestProjects/Sequential/Sequential.cs
@@ -0,0 +1,65 @@
+﻿public class SequencedTests
+{
+    private readonly int a = 10;
+    private readonly int b = 10;
+    private readonly int c = 5;
+    private int counter = 0;
+
+    private void FunctionThatThrowsException()
+    {
+        throw new SystemException();
+    }
+
+    [MyNUnit.Attributes.BeforeClass]
+    public static void BeforeClassMethod()
+    {
+
+    }
+
+    [MyNUnit.Attributes.Before]
+    public void Before()
+    {
+    }
+
+    [MyNUnit.Attributes.AfterClass]
+    public static void AfterClassMethod()
+    {
+
+    }
+
+    [MyNUnit.Attributes.Test]
+    public void TestShouldPass()
+    {
+        MyNUnit.Assertions.Assert.That(a == b);
+    }
+
+    [MyNUnit.Attributes.Test]
+    public void TestShouldFail()
+    {
+        MyNUnit.Assertions.Assert.That(a == c, "Values are not equal");
+    }
+
+    [MyNUnit.Attributes.Test]
+    public void ExceptionIsNotExpected()
+    {
+        FunctionThatThrowsException();
+    }
+
+    [MyNUnit.Attributes.Test]
+    public void ExceptionIsExpected()
+    {
+        MyNUnit.Assertions.Assert.Expected<SystemException>(FunctionThatThrowsException);
+    }
+
+    [MyNUnit.Attributes.Test]
+    public void IncorrectException()
+    {
+        MyNUnit.Assertions.Assert.Expected<InvalidDataException>(FunctionThatThrowsException);
+    }
+
+    [MyNUnit.Attributes.Test(IgnoreMessage = "This test should be ignored")]
+    public void IgnoredTest()
+    {
+       throw new NotImplementedException();
+    }
+}
